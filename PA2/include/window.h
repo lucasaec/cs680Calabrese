9236219dc7e644f2b862bef5f7006cfd54a49a50
lucasaec <lucaseac@hawaii.edu>
2018-09-12 00:22:08 -0700

    Updating Files for PA2

---
+++ PA2/include/window.h
@@ -0,0 +1,23 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <SDL2/SDL.h>
+#include <string>
+using namespace std;
+
+class Window
+{
+  public:
+    Window();
+    ~Window();
+    bool Initialize(const string &name, int* width, int* height);
+    void Swap();
+    SDL_Window* GetWindow();
+    SDL_GLContext GetgContext();
+
+  private:
+    SDL_Window* gWindow;
+    SDL_GLContext gContext;
+};
+
+#endif /* WINDOW_H */
