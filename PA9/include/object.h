fb67f6f064034d2bdec676a9989f37988be59cd0
andrewflangas <42788486+andrewflangas@users.noreply.github.com>
2018-11-05 00:14:10 +0000

    Add files via upload

---
+++ PA9/include/object.h
@@ -0,0 +1,36 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+#include <btBulletDynamicsCommon.h>
+class Object
+{
+  public:
+    Object();
+    Object(std::string, float, float, float, float, int);
+    ~Object();
+    void Update(unsigned int dt);
+    void Render();
+    std::vector<Object*> children;
+    int level;
+    float scale1;
+    Object* parent;
+    glm::mat4 GetModel();
+    glm::mat4 model;
+    btVector3 triArray[3];
+    btTriangleMesh *objTriMesh;
+    btCollisionShape *shape;
+    btRigidBody *rigidBody;
+    int physics;
+  private:
+    
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+
+    float angle;
+};
+
+#endif /* OBJECT_H */
